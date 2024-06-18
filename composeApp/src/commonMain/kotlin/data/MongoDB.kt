99e1847112d60ef6bee143b257877f9891bc7132
NicolasLV23 <nicolas.lazarte@fractale.vision>
2024-06-18 16:49:47 -0400

    Création de la bd sur MongoDB

---
+++ composeApp/src/commonMain/kotlin/data/MongoDB.kt
@@ -0,0 +1,50 @@
+package data
+
+import domain.RequestState
+import domain.ToDoTask
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+
+class MongoDB {
+    var realm: Realm? = null
+
+    init {
+        configureTheRealm()
+    }
+
+    private fun configureTheRealm() {
+        if (realm == null || realm!!.isClosed()) {
+            val config = RealmConfiguration.Builder(
+                schema = setOf(ToDoTask::class)
+            )
+                .compactOnLaunch()
+                .build()
+            realm = Realm.open(config)
+        }
+    }
+
+    fun readActiveTasks(): Flow<RequestState<List<ToDoTask>>> {
+        return realm?.query<ToDoTask>(query = "completed == $0", false)
+            ?.asFlow()
+            ?.map { result ->
+                RequestState.Success(
+                    data = result.list.sortedByDescending { task -> task.favorite }
+                )
+            } ?: flow { RequestState.Error(message = "Realm is not available.") }
+    }
+
+    fun readCompletedTasks(): Flow<RequestState<List<ToDoTask>>> {
+        return realm?.query<ToDoTask>(query = "completed == $0", true)
+            ?.asFlow()
+            ?.map { result -> RequestState.Success(data = result.list) }
+            ?: flow { RequestState.Error(message = "Realm is not available.") }
+    }
+
+    suspend fun addTask(task: ToDoTask) {
+        realm?.write { copyToRealm(task) }
+    }
+}
